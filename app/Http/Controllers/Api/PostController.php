5801026ed59a9d2d4f5e287f529023ae20919036
Alexander Steingold <alex461976@gmail.com>
2023-06-12 18:42:21 +0300

    12.06.23 part 2

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\StoreRequest;
 use App\Http\Requests\Post\UpdateRequest;
+use App\Http\Resources\Post\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -14,12 +15,13 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
-        return view('posts.index', compact('posts'));
+        return PostResource::collection($posts)->resolve();
     }
+
 
     public function show(Post $post)
     {
-        return view('posts.show', compact('post'));
+        return PostResource::make($post)->resolve();
     }
 
     public function store(StoreRequest $request)
@@ -30,7 +32,8 @@ class PostController extends Controller
             $data['image'] = $path;
         }
 
-        Post::create($data);
+        $post = Post::create($data);
+        return PostResource::make($post)->resolve();
     }
 
     public function update(UpdateRequest $request, Post $post)
@@ -42,11 +45,15 @@ class PostController extends Controller
         }
 
         $post->update($data);
+        return PostResource::make($post)->resolve();
     }
 
     public function destroy(Post $post)
     {
         $post->delete();
+        return response()->json([
+            'message' => 'deleted'
+        ]);
     }
 
 }
